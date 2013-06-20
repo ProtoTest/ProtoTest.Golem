5bd725fb5c1532c37131e0d33be8c545889b6dee
Brian Kitchener <bkitchener@soasta.com>
2013-06-20 13:55:16 -0600

    adding ieggplantdriver class

---
+++ Golem.Framework/IEggPlantDriver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CookComputing.XmlRpc;
+
+namespace Golem.Framework
+{
+    [XmlRpcUrl("http://127.0.0.1:5400")]
+    public interface IEggPlantDriver : IXmlRpcProxy
+    {
+        [XmlRpcMethod("StartSession")]
+        dynamic StartSession(string suitePath);
+
+        [XmlRpcMethod("Execute")]
+        XmlRpcStruct Execute(string command);
+
+        [XmlRpcMethod("EndSession")]
+        dynamic EndSession();
+    }
+}
