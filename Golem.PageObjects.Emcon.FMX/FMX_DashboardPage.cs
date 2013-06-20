280079a866c083dbcf7aa1e4d65a6a2d6a17b407
Seth <surban@prototest.com>
2013-06-20 13:46:18 -0600

    fucking go

---
+++ Golem.PageObjects.Emcon.FMX/FMX_DashboardPage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Golem.Framework;
+using OpenQA.Selenium;
+
+namespace Golem.PageObjects.Emcon.FMX
+{
+    class FMX_DashboardPage : FmxMenuBar
+    {
+        Element dd_TeamSelector = new Element("TeamSelectionDropDown", By.Id("ctl00_ContentPlaceHolder1_ucDailyReports_ddlDailyTeam"));
+
+    }
+}
