15a2498c73c8764b7884518a51b69ac5d2f5d9b2
Matt <msiwiec@yahoo.com>
2013-09-26 13:26:12 -0600

    added element with form validation

--- Golem.PageObjects.Cael/MyAccount/ProfilePage.cs
+++ Golem.PageObjects.Cael/MyAccount/ProfilePage.cs
@@ -10,24 +10,37 @@ namespace Golem.PageObjects.Cael.MyAccount
 {
     public class ProfilePage : MyAccountPage
     {
-        public Element EducationAchieved_Drp = new Element("Education Achieved Dropdowqn", By.Id("p_lt_ctl02_pageplaceholder_p_lt_ctl00_MyAccount_1_educationAchievedDDList"));
+        public ValidationElement EducationAchieved_Drp = new ValidationElement("Education Achieved Dropdowqn", 
+                                                                                By.Id("p_lt_ctl02_pageplaceholder_p_lt_ctl00_MyAccount_1_educationAchievedDDList"),
+                                                                                By.XPath("//*[@class='p_lt_ctl02_pageplaceholder_p_lt_ctl00_MyAccount_1_educationAchievedDDListformError parentFormform formError']/div"));
         public Element AreaOfStudy_Drp = new Element("Area of Study", By.Id("p_lt_ctl02_pageplaceholder_p_lt_ctl00_MyAccount_1_areaOfStudyDDList"));
-        public Element NameOfCollege_Drp = new Element("Name of College", By.Id("schoolDDList"));
+        public ValidationElement NameOfCollege_Drp = new ValidationElement("Name of College",
+                                                                           By.Id("schoolDDList"),
+                                                                           By.XPath("//*[@class='schoolDDListformError parentFormform formError']/div"));
         public Element CollegeCredit_Radio = new Element("College Credit Interest Radio", By.Id("p_lt_ctl02_pageplaceholder_p_lt_ctl00_MyAccount_1_investigationRButton_0"));
         public Element CollegeCreditsEarned_Drp = new Element("College Credits Earned Dropdown", By.Id("p_lt_ctl02_pageplaceholder_p_lt_ctl00_MyAccount_1_creditsEarnedDDList"));
         public Element CollegeCreditsNeed_Drp = new Element("College Credits Needed Dropdown", By.Id("p_lt_ctl02_pageplaceholder_p_lt_ctl00_MyAccount_1_creditsRemainingDDList"));
         public Element InterestedOnline_Radio = new Element("InterestedOnline_Radio", By.Id("p_lt_ctl02_pageplaceholder_p_lt_ctl00_MyAccount_1_onlineRButton_0"));
         public Element FinancialAid_Radio = new Element("FinancialAid_Radio", By.Id("p_lt_ctl02_pageplaceholder_p_lt_ctl00_MyAccount_1_aidRButton_0"));
         public Element TopicOfInterest_Drp = new Element("Topic of Interest", By.Id("p_lt_ctl02_pageplaceholder_p_lt_ctl00_MyAccount_1_topicOfInterestDDList"));
-        public Element Gender_Radio = new Element("Gender_Radio", By.Id("genderRButton_0"));
-        public Element Race_Drp = new Element("Race_Drp", By.Id("p_lt_ctl02_pageplaceholder_p_lt_ctl00_MyAccount_1_EthnicityDDList"));
-        public Element MilitaryExperience_Drp = new Element("MilitaryExperience_Drp", By.Id("p_lt_ctl02_pageplaceholder_p_lt_ctl00_MyAccount_1_militaryDDList"));
-        public Element EmploymentStatus_Drp = new Element("EmploymentStatus_Drp", By.Id("p_lt_ctl02_pageplaceholder_p_lt_ctl00_MyAccount_1_employmentStatusDDList"));
+        public ValidationElement Gender_Radio = new ValidationElement("Gender_Radio",
+                                                                      By.Id("genderRButton_0"),
+                                                                      By.XPath("//*[@class='genderRButton_0formError parentFormform formError']/div"));
+        public ValidationElement Race_Drp = new ValidationElement("Race_Drp",
+                                                                  By.Id("p_lt_ctl02_pageplaceholder_p_lt_ctl00_MyAccount_1_EthnicityDDList"),
+                                                                  By.XPath("//*[@class='p_lt_ctl02_pageplaceholder_p_lt_ctl00_MyAccount_1_EthnicityDDListformError parentFormform formError']/div"));
+        public ValidationElement MilitaryExperience_Drp = new ValidationElement("MilitaryExperience_Drp",
+                                                                                By.Id("p_lt_ctl02_pageplaceholder_p_lt_ctl00_MyAccount_1_militaryDDList"),
+                                                                                By.XPath("//*[@class='p_lt_ctl02_pageplaceholder_p_lt_ctl00_MyAccount_1_militaryDDListformError parentFormform formError']/div"));
+        public ValidationElement EmploymentStatus_Drp = new ValidationElement("EmploymentStatus_Drp",
+                                                                              By.Id("p_lt_ctl02_pageplaceholder_p_lt_ctl00_MyAccount_1_employmentStatusDDList"),
+                                                                              By.XPath("//*[@class='p_lt_ctl02_pageplaceholder_p_lt_ctl00_MyAccount_1_employmentStatusDDListformError parentFormform formError']/div"));
         public Element Employer_Field = new Element("Employer_Field", By.Id("employerTextBox"));
         public Element TuitionAssistance_Radio = new Element("TuitionAssistance_Radio", By.Id("p_lt_ctl02_pageplaceholder_p_lt_ctl00_MyAccount_1_tuitionRButton_0"));
 
-        public Element AnnualIncome_Drp = new Element("AnnualIncome_Drp",
-            ByE.PartialAttribute("select", "@id", "annualPersonalIncomeDDList"));
+        public ValidationElement AnnualIncome_Drp = new ValidationElement("AnnualIncome_Drp",
+                                                                          ByE.PartialAttribute("select", "@id", "annualPersonalIncomeDDList"),
+                                                                          By.XPath("//*[@class='p_lt_ctl02_pageplaceholder_p_lt_ctl00_MyAccount_1_annualPersonalIncomeDDListformError parentFormform formError']/div"));
         public Element LaborUnion_Radio = new Element("LaborUnion_Radio", By.Id("p_lt_ctl02_pageplaceholder_p_lt_ctl00_MyAccount_1_laborRButton_0"));
         public Element ReceivedTraining_Radio = new Element("ReceivedTraining_Radio", By.Id("p_lt_ctl02_pageplaceholder_p_lt_ctl00_MyAccount_1_trainingRButton_0"));
         public Element NameOrTypeOfTrainnig_Field = new Element("NameOrTypeOfTrainnig_Field", By.Id("p_lt_ctl02_pageplaceholder_p_lt_ctl00_MyAccount_1_trainingTypeTextBox"));
@@ -98,6 +111,20 @@ namespace Golem.PageObjects.Cael.MyAccount
 
         }
 
+        public ProfilePage VerifyProfileFormValidations(string education, string nameOfCollege, string gender,
+            string race, string militaryExperience, string employmentStatus, string annualIncome)
+        {
+            EducationAchieved_Drp.VerifyTextValidation(education);
+            NameOfCollege_Drp.VerifyTextValidation(nameOfCollege);
+            Golem.Framework.Common.Log("ATTEMPTING TO CLEAR GENDER RADIO!!!!!");
+            Gender_Radio.VerifyTextValidation(gender); // NOT FINDING THIS ELEMENT? Also how can we uncheck the radio?
+            Race_Drp.VerifyTextValidation(race);
+            MilitaryExperience_Drp.VerifyTextValidation(militaryExperience);
+            EmploymentStatus_Drp.VerifyTextValidation(employmentStatus);
+            AnnualIncome_Drp.VerifyTextValidation(annualIncome);
+
+            return this;
+        }
 
         public override void WaitForElements()
         {
