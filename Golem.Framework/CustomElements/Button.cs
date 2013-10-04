498f75a98e7bee5bcc007d1528dbff360718448d
Brian Kitchener <bkitchener@soasta.com>
2013-10-04 11:38:16 -0600

    Adding custom element classes

---
+++ Golem.Framework/CustomElements/Button.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Golem.Framework.CustomElements
+{
+    public class Button : Element
+    {
+
+    }
+}
