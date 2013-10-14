54c86437ed8255b757791d01fb51000e224fe010
Matt <msiwiec@yahoo.com>
2013-10-14 12:41:32 -0600

    added Assess portfolio and submit test

--- Golem.Tests.Cael/AssessmentTests.cs
+++ Golem.Tests.Cael/AssessmentTests.cs
@@ -12,15 +12,40 @@ namespace Golem.Tests.Cael
 {
     class AssessmentTests : TestBaseClass
     {
-        string email = "prototestassessor@mailinator.com";
-        string password = "prototest123!!";
+        //string email = "prototestassessor@mailinator.com";
+        //string password = "prototest123!!";
 
         [Test]
         void VerifySliderValueRecommendations()
         {
+            // UserTests.email2 is the assessor
             HomePage.OpenHomePage().
                 GoToLoginPage().
-                Login(email, password).Header.GoToDashboardPageForAssessor().SelectPendingAssessment().VerifyRecommendationSliderValues();
+                Login(UserTests.email2, UserTests.password).Header.GoToDashboardPageForAssessor().SelectPendingAssessment().VerifyRecommendationSliderValues();
         }
+
+        [Test]
+        void AssessPortfolioAndSubmitTest()
+        {
+            int[] slider_no_credit_values = {4,4,4,4,1,1,1}; // 19
+            int[] slider_credit_values = {4,4,4,4,4,1,1}; // 22
+
+            HomePage.OpenHomePage().
+                GoToLoginPage().
+                Login(UserTests.email2, UserTests.password).Header
+                    .GoToDashboardPageForAssessor()
+                    .SelectPendingAssessment()
+                    .SetSliderValues(slider_no_credit_values)
+                    .ReviewAssessmentNoCredit()
+                    .ClickCancel()
+                    .ReviewAssessmentNoCredit()
+                    .ReviewAssessmentConfirm(true).VerifyCreditRecommendation("19/28", false)
+                    .GoBackAndEditAssessment().SetSliderValues(slider_credit_values)
+                    .ReviewAssessmentCredit()
+                    .VerifyCreditRecommendation("22/28", true)
+                    .SubmitAssessment();
+                    
+        }
+       
     }
 }
