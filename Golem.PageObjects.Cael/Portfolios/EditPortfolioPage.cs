6c4997d637d4f77bbe08b910a51ed9459c9dfc86
Matt <msiwiec@yahoo.com>
2013-10-22 10:28:18 -0600

    search for elements by partial text, fix test

--- Golem.PageObjects.Cael/Portfolios/EditPortfolioPage.cs
+++ Golem.PageObjects.Cael/Portfolios/EditPortfolioPage.cs
@@ -19,11 +19,11 @@ namespace Golem.PageObjects.Cael.Setup_Portfolio
         public Element LearningNarrative_FileChooser = new Element("LearningNarrative_FileChooser", By.Id("fileupload"));
         public Element AddAnotherDocument_Link = new Element("AddAnotherDocument_Link", By.LinkText("Add another document"));
         public Element Cancel_Link = new Element("Cancel_Link", By.Id("p_lt_ctl02_pageplaceholder_p_lt_ctl00_LC_EditPortfolio_cancelHyperLink"));
-        public Element SaveChanges_Button = new Element("SaveChanges_Button", By.XPath("//button[text()='Save Changes']"));
+        public Element SaveChanges_Button = new Element("SaveChanges_Button", ByE.PartialText("Save Changes"));
         public Element SubmitPortfolio_Button = new Element("SubmitPortfolio_Button", By.Id("submitPortfolioButton"));
 
         public Element SupportLinkUrl_Field = new Element("LinkUrl Field",By.Name("link_url"));
-        public Element SupportFileCaption_Field = new Element("LinkUrl Field", By.Name("course_description_link"));
+        public Element SupportFileCaption_Field = new Element("Support File Caption Field", By.Name("course_description_link"));
         public Element SupportDescription_Field = new Element("Support Description Field", By.XPath("//textarea[@ng-model='supportingDocument.description']"));
         public Element SupportSave_Button = new Element("Support save button", By.XPath("//button[text()='Save']"));
         public Element FileUpload_FileChooser = new Element("File UPload file cho oser", By.Id("file"));
@@ -39,13 +39,14 @@ namespace Golem.PageObjects.Cael.Setup_Portfolio
             driver.FindVisibleElement(By.Name("link_url")).SendKeys(url);
             driver.FindVisibleElement(By.Name("course_description_link")).SendKeys(caption);
             driver.FindVisibleElement(By.XPath("//textarea[@ng-model='supportingDocument.description']")).SendKeys(descrtiption);
-            driver.FindVisibleElement(By.XPath("//button[text()='Save']")).Click();
+            //ByE.PartialAttribute("button", "text()", "Save");
+            driver.FindVisibleElement(By.XPath("//button[contains(text(),'Save')]")).Click();
             return this;
         }
 
         public EditPortfolioPage EditOutcomesText(string text)
         {
-            driver.SwitchTo().Frame(driver.FindElement(By.XPath("//iframe"))).FindElement(By.TagName("body")).SendKeys(text);
+            driver.SwitchTo().Frame(driver.WaitForVisible(By.XPath("//iframe")).FindElement(By.XPath("//iframe"))).FindElement(By.TagName("body")).SendKeys(text);
             driver.SwitchTo().DefaultContent();
             return this;
         }
