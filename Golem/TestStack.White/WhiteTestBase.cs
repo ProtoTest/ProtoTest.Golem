f7d13acb8cf97488b3c09d0b2b4babcfe9b9c9de
Seth <surban@prototest.com>
2013-12-11 11:26:15 -0700

    Removed Component Class From TestStack

--- Golem/TestStack.White/WhiteTestBase.cs
+++ Golem/TestStack.White/WhiteTestBase.cs
@@ -5,6 +5,7 @@ using System.Text;
 using MbUnit.Framework;
 using Gallio.Framework;
 using TestStack.White;
+using TestStack.White.UIItems.WindowItems;
 using ProtoTest.Golem.Core;
 
 namespace Golem.TestStack.White
@@ -12,11 +13,18 @@ namespace Golem.TestStack.White
     public class WhiteTestBase : TestBase
     {
         public static Application _app;
+        public static Window window;
 
         [SetUp]
         public void LaunchApp()
         {
             _app = Application.Launch("C:\\Program Files\\Quest Integrity Group\\LifeQuest Pipeline\\LifeQuest.exe");
+        }
+
+        [TearDown]
+        public void CloseApplication()
+        {
+            if (_app != null) _app.Kill();
         }
 
         
