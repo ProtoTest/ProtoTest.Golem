4382578abcf9bb441dba2318f3414f329aeaf041
Seth <surban@prototest.com>
2014-01-09 11:20:01 -0700

    added new white elements listview

---
+++ ProtoTest.Golem/White/Elements/WhiteListView.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Automation;
+using TestStack.White;
+using TestStack.White.Recording;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Actions;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.ListViewItems;
+using TestStack.White.UIItems.Scrolling;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.White.WindowsAPI;
+using Action = TestStack.White.UIItems.Actions.Action;
+using Point = System.Windows.Point;
+
+namespace ProtoTest.Golem.White.Elements
+{
+    public class WhiteListView : ListView
+    {
+        public string Description;
+        public SearchCriteria Criteria;
+        public Window Window;
+        private ListView _item;
+        private UIItem Parent;
+
+        public ListView item
+        {
+            get
+            {
+                _item = ElementFactory.GetItem(_item, Criteria, Window, Parent);
+                return _item;
+            }
+            set
+            {
+                _item = value;
+            }
+        }
+
+        public WhiteListView(string Description, SearchCriteria Criteria, Window Window = null)
+        {
+            this.Description = Description;
+            this.Criteria = Criteria;
+            this.Window = Window ?? WhiteTestBase.window;
+        }
+
+        public WhiteListView(string Description, SearchCriteria Criteria, UIItem Parent)
+        {
+            this.Description = Description;
+            this.Criteria = Criteria;
+            this.Window = null;
+        }
+
+        public override void Select(int zeroitemdRowIndex)
+        {
+            item.Select(zeroitemdRowIndex);
+        }
+
+        public override void MultiSelect(int zeroitemdRowIndex)
+        {
+            item.MultiSelect(zeroitemdRowIndex);
+        }
+
+        public override ListViewCell Cell(string column, int zeroitemdRowIndex)
+        {
+            return item.Cell(column, zeroitemdRowIndex);
+        }
+
+        public override void Select(string column, string value)
+        {
+            item.Select(column, value);
+        }
+
+        public override void MultiSelect(string column, string value)
+        {
+            item.MultiSelect(column, value);
+        }
+
+        public override ListViewRow Row(string column, string value)
+        {
+            return item.Row(column, value);
+        }
+
+        public override void TryUnSelectAll()
+        {
+            item.TryUnSelectAll();
+        }
+
+        public override void HookEvents(UIItemEventListener eventListener)
+        {
+            item.HookEvents(eventListener);
+        }
+
+        public override void UnHookEvents()
+        {
+            item.UnHookEvents();
+        }
+
+        public override void Select(string text)
+        {
+            item.Select(text);
+        }
+
+        public override void ActionPerforming(UIItem uiItem)
+        {
+            item.ActionPerforming(uiItem);
+        }
+
+        public override ListViewRows Rows
+        {
+            get { return item.Rows; }
+        }
+
+        public override ListViewHeader Header
+        {
+            get { return item.Header; }
+        }
+
+        public override ListViewRows SelectedRows
+        {
+            get { return item.SelectedRows; }
+        }
+
+        public override List<string> Items
+        {
+            get { return item.Items; }
+        }
+
+        public override VerticalSpan VerticalSpan
+        {
+            get { return item.VerticalSpan; }
+        }
+
+       public override bool ValueOfEquals(AutomationProperty property, object compareTo)
+        {
+            return item.ValueOfEquals(property, compareTo);
+        }
+
+        public override void RightClickAt(Point point)
+        {
+            item.RightClickAt(point);
+        }
+
+        public override void RightClick()
+        {
+            item.RightClick();
+        }
+
+        public override void Focus()
+        {
+            item.Focus();
+        }
+
+        public override void Visit(WindowControlVisitor windowControlVisitor)
+        {
+            item.Visit(windowControlVisitor);
+        }
+
+        public override string ErrorProviderMessage(Window window)
+        {
+            return item.ErrorProviderMessage(window);
+        }
+
+        public override bool NameMatches(string text)
+        {
+            return item.NameMatches(text);
+        }
+
+        public override void Click()
+        {
+            item.Click();
+        }
+
+        public override void DoubleClick()
+        {
+            item.DoubleClick();
+        }
+
+        public override void KeyIn(KeyboardInput.SpecialKeys key)
+        {
+            item.KeyIn(key);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return item.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return item.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return item.ToString();
+        }
+
+        public override void SetValue(object value)
+        {
+            item.SetValue(value);
+        }
+
+        public override void ActionPerformed(Action action)
+        {
+            item.ActionPerformed(action);
+        }
+
+        public override void LogStructure()
+        {
+            item.LogStructure();
+        }
+
+        public override AutomationElement GetElement(SearchCriteria searchCriteria)
+        {
+            return item.GetElement(searchCriteria);
+        }
+
+        public override void Enter(string value)
+        {
+            item.Enter(value);
+        }
+
+        public override void RaiseClickEvent()
+        {
+            item.RaiseClickEvent();
+        }
+
+        public override AutomationElement AutomationElement
+        {
+            get { return item.AutomationElement; }
+        }
+
+        public override bool Enabled
+        {
+            get { return item.Enabled; }
+        }
+
+        public override WindowsFramework Framework
+        {
+            get { return item.Framework; }
+        }
+
+        public override Point Location
+        {
+            get { return item.Location; }
+        }
+
+        public override Rect Bounds
+        {
+            get { return item.Bounds; }
+        }
+
+        public override string Name
+        {
+            get { return item.Name; }
+        }
+
+        public override Point ClickablePoint
+        {
+            get { return item.ClickablePoint; }
+        }
+
+        public override string AccessKey
+        {
+            get { return item.AccessKey; }
+        }
+
+        public override string Id
+        {
+            get { return item.Id; }
+        }
+
+        public override bool Visible
+        {
+            get { return item.Visible; }
+        }
+
+        public override string PrimaryIdentification
+        {
+            get { return item.PrimaryIdentification; }
+        }
+
+        public override ActionListener ActionListener
+        {
+            get { return item.ActionListener; }
+        }
+
+        public override IScrollBars ScrollBars
+        {
+            get { return item.ScrollBars; }
+        }
+
+        public override bool IsOffScreen
+        {
+            get { return item.IsOffScreen; }
+        }
+
+        public override bool IsFocussed
+        {
+            get { return item.IsFocussed; }
+        }
+
+        public override Color BorderColor
+        {
+            get { return item.BorderColor; }
+        }
+
+        public override Bitmap VisibleImage
+        {
+            get { return item.VisibleImage; }
+        }
+
+        public override string HelpText
+        {
+            get { return item.HelpText; }
+        }
+    }
+}
