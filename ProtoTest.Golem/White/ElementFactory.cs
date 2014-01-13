5d1a98729cf5890716fef0c01d6230c3ee3e2c5e
bkitchener1 <kitchener.brian@gmail.com>
2014-01-13 11:00:49 -0700

    cleaning up logic

--- ProtoTest.Golem/White/ElementFactory.cs
+++ ProtoTest.Golem/White/ElementFactory.cs
@@ -1,37 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Gallio.Framework;
-using ProtoTest.Golem.Core;
+﻿using ProtoTest.Golem.Core;
+using ProtoTest.Golem.White.Elements;
 using TestStack.White.UIItems;
 using TestStack.White.UIItems.Finders;
-using TestStack.White.UIItems.WindowItems;
 using TestStack.White.UIItems.WPFUIItems;
 
-namespace ProtoTest.Golem.White.Elements
+namespace ProtoTest.Golem.White
 {
-    public class ElementFactory
+    public class ElementFactory 
     {
         private static string LogFormatString = "{0}Looking for item in '{1}' with '{2}'";
         public static T GetItem<T>(T item, SearchCriteria criteria, UIItem parent) where T : UIItem
         {
-            if (item == null || item.IsStale())
-            {
-                if (parent.GetType() == typeof (WhiteWindow))
-                {
-                    var window = (WhiteWindow)parent;
-                    if (window == null) window = WhiteTestBase.window;
-                    TestBase.LogEvent(string.Format(LogFormatString, WhiteTestBase.GetCurrentClassAndMethodName(), window.description, criteria));
-                    item = window.Get<T>(criteria);
-                }
-                else
-                {
-                    TestBase.LogEvent(string.Format(LogFormatString, ((IWhiteElement)parent).description, criteria));
-                    item = parent.Get<T>(criteria);
-                }            
-            }
+
+            if (item != null && !item.IsStale()) return item;
+            string description = parent.GetType() == typeof(WhiteWindow) ? ((WhiteWindow)parent).description : ((IWhiteElement)parent).description;
+            TestBase.LogEvent(string.Format(LogFormatString, WhiteTestBase.GetCurrentClassAndMethodName(), description, criteria));
+            item = parent.Get<T>(criteria);
             return item;
         }
+
     }
 }
