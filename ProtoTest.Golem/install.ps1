8b84b5c1bdb915322c53def75d2471524ef80030
andar <andartagg@gmail.com>
2014-02-03 15:09:42 -0700

    Add files for building the nuget package.

---
+++ ProtoTest.Golem/install.ps1
@@ -0,0 +1,20 @@
+param($installPath, $toolsPath, $package, $project)
+Write-Host "InstallPath: $installPath"
+Write-Host "ToolsPath: $toolsPath"
+Write-Host "Package: $package"
+Write-Host "Project: $project"
+
+$CopyChromeDriver = "`nxcopy /y `"`$(ProjectDir)chromedriver.exe`" `"`$(TargetDir)`""
+$CopyIEDriver = "`nxcopy /y `"`$(ProjectDir)IEDriverServer.exe`" `"`$(TargetDir)`""
+
+
+# Get the current Post Build Event cmd
+$currentPostBuildCmd = $project.Properties.Item("PostBuildEvent").Value
+
+# Append our post build command if it's not already there
+if (!$currentPostBuildCmd.Contains($CopyChromeDriver)) {
+    $project.Properties.Item("PostBuildEvent").Value += $CopyChromeDriver
+}
+if (!$currentPostBuildCmd.Contains($CopyIEDriver)) {
+    $project.Properties.Item("PostBuildEvent").Value += $CopyIEDriver
+}
