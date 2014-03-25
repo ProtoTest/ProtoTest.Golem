a36e035018ff14b4a79d9c832321a4a9b3673dd1
Seth <surban@prototest.com>
2014-03-25 16:52:17 -0600

    Added More PurpleElements

---
+++ ProtoTest.Golem/Purple/PurpleElements/PurpleDropDown.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Automation;
+
+namespace ProtoTest.Golem.Purple.PurpleElements
+{
+    public class PurpleDropDown : PurpleElementBase
+    {
+        private AutomationElementCollection availableOptions;
+
+        public PurpleDropDown(string name, string locatorPath) : base(name, locatorPath)
+        {
+        }
+
+        //change to get selected item
+        public string GetSelected()
+        {
+            string nameofSelected = "";
+            object basePattern;
+            if (PurpleElement.TryGetCurrentPattern(SelectionPattern.Pattern, out basePattern))
+            {
+                SelectionPattern selection = (BasePattern) basePattern as SelectionPattern;
+                if (selection != null)
+                {
+                    AutomationElement[] automationElements = selection.Current.GetSelection();
+                    foreach (var automationElement in automationElements)
+                    {
+                        nameofSelected = automationElement.Current.Name;
+                    }
+                }
+            }
+            return nameofSelected;
+        }
+
+        public void SelectItem(string item)
+        {
+            //Now we need to find the right one
+            int matchingIndex = -1;
+            AutomationElement itemToSelect = null;
+
+            object basePattern;
+            if (PurpleElement.TryGetCurrentPattern(ExpandCollapsePattern.Pattern, out basePattern))
+            {
+                ExpandCollapsePattern expand = (BasePattern)basePattern as ExpandCollapsePattern;
+                if (expand != null)
+                {
+                    if (expand.Current.ExpandCollapseState == ExpandCollapseState.Collapsed)
+                    {
+                        expand.Expand();
+                        
+                        availableOptions = PurpleElement.FindAll(TreeScope.Subtree, new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.ListItem));
+                        for (int x = 0; x < availableOptions.Count; x++)
+                        {
+                            if (item == availableOptions[x].Current.Name)
+                            {
+                                itemToSelect = availableOptions[x];
+                            }
+                        }
+                        if (itemToSelect != null)
+                        {
+                           SelectionItemPattern selectPattern = (SelectionItemPattern)itemToSelect.GetCurrentPattern(SelectionItemPattern.Pattern);
+                            selectPattern.Select(); 
+                        }
+                    }
+
+                }
+            }
+            
+
+            
+
+        }
+
+    }
+}
+
---
+++ ProtoTest.Golem/Purple/PurpleElements/PurpleRadioButton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProtoTest.Golem.Purple.PurpleElements
+{
+    public class PurpleRadioButton : PurpleElementBase
+    {
+        public PurpleRadioButton(string name, string locatorPath) : base(name, locatorPath)
+        {
+        }
+
+        //TODO Figure out how to get selected item value
+
+    }
+}
