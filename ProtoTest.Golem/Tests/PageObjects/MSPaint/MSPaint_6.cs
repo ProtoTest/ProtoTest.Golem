327277f48e40dc2e1cc490c420106260f448b0d9
Seth <surban@prototest.com>
2014-03-27 14:53:29 -0600

    Added Example MS paint Test

---
+++ ProtoTest.Golem/Tests/PageObjects/MSPaint/MSPaint_6.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ProtoTest.Golem.Purple;
+using ProtoTest.Golem.Purple.PurpleElements;
+
+namespace ProtoTest.Golem.Tests.PageObjects.MSPaint
+{
+    public class MSPaint_6 : BaseScreenObject
+    {
+        PurpleButton TextButton = new PurpleButton("MSPaintTextButton", "Untitled - Paint/Ribbon/Ribbon/!BLANK!/Ribbon/Lower Ribbon/!BLANK!/Home/Tools/Text");
+        PurplePanel PaintArea = new PurplePanel("PaintArea", "Untitled - Paint/!BLANK!/!BLANK!");
+
+        public void PaintText(string text)
+        {
+            TextButton.Click();
+            PaintArea.MoveCursor(PaintArea.Bounds.TopLeft);
+            PaintArea.LMB_Down();
+            SendKeys.SendWait(text);
+        }
+
+        public static MSPaint_6 PaintWindow()
+        {
+            return new MSPaint_6();
+        }
+    }
+}
---
+++ ProtoTest.Golem/Tests/TestMSPaint6.cs
@@ -0,0 +1,27 @@
+﻿using MbUnit.Framework;
+using ProtoTest.Golem.Core;
+using ProtoTest.Golem.Purple;
+using ProtoTest.Golem.Tests.PageObjects.MSPaint;
+
+namespace ProtoTest.Golem.Tests
+{
+    public class TestMSPaint6 : PurpleTestBase
+    {
+        [FixtureInitializer]
+        public void setup()
+        {
+            Config.Settings.purpleSettings.Purple_windowTitle = "Untitled - Paint";
+            Config.Settings.purpleSettings.ProcessName = "Paint";
+            Config.Settings.purpleSettings.Purple_blankValue = "!BLANK!";
+            Config.Settings.purpleSettings.Purple_Delimiter = "/";
+            Config.Settings.purpleSettings.appPath = "%windir%\\system32\\mspaint.exe";
+            Config.Settings.purpleSettings.Purple_ElementTimeoutWaitSeconds = 30;
+        }
+
+        [Test]
+        public void PaintExample()
+        {
+            MSPaint_6.PaintWindow().PaintText("ProtoTest Purple");
+        }
+    }
+}
