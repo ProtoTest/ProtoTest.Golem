cdeb4a0251ff252e27c6007de1efe8535565153b
Brian Kitchener <kitchener.brian@gmail.com>
2016-05-17 16:26:51 -0600

    adding verifications

--- ProtoTest.Golem/WebDriver/Components.cs
+++ ProtoTest.Golem/WebDriver/Components.cs
@@ -8,7 +8,7 @@ using ProtoTest.Golem.Core;
 
 namespace ProtoTest.Golem.WebDriver
 {
-    public class Components<T> : IEnumerable<T> where T : BaseComponent, new()
+    public class Components<T> : IEnumerable<T> where T : Element, new()
     {
        private Element RootElements;
 
@@ -34,6 +34,44 @@ namespace ProtoTest.Golem.WebDriver
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
+        }
+
+        /// <summary>
+        ///     Create an element verification for some condition.
+        /// </summary>
+        /// <returns>A new ElementVerification for the element</returns>
+        public ElementVerification Verify()
+        {
+           var  timeoutSec = Config.settings.runTimeSettings.ElementTimeoutSec;
+            return new ElementVerification(this.RootElements, timeoutSec, false);
+        }
+
+        /// <summary>
+        ///     Wait for some condition on the element
+        /// </summary>
+        /// <returns>A new ElementVerification for the element</returns>
+        public ElementVerification WaitUntil()
+        {
+            var timeoutSec = Config.settings.runTimeSettings.ElementTimeoutSec;
+            return new ElementVerification(this.RootElements, timeoutSec, true);
+        }
+
+        /// <summary>
+        ///     Create an element verification for some condition.
+        /// </summary>
+        /// <returns>A new ElementVerification for the element</returns>
+        public ElementVerification Verify(int timeoutSec)
+        {
+            return new ElementVerification(this.RootElements, timeoutSec, false);
+        }
+
+        /// <summary>
+        ///     Wait for some condition on the element
+        /// </summary>
+        /// <returns>A new ElementVerification for the element</returns>
+        public ElementVerification WaitUntil(int timeoutSec)
+        {
+            return new ElementVerification(this.RootElements, timeoutSec, true);
         }
     }
 }
